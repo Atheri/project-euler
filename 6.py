527c47ad02ff217cf3ce20c45c0c325bac715940
Atheri <corylotze@gmail.com>
2015-07-19 04:08:55 -0700

    Problem 6 done

---
+++ 6.py
@@ -0,0 +1,13 @@
+
+sum_of_squares = 0
+square_of_sums = 0
+
+for i in range(1,101):
+    sum_of_squares += i**2
+    square_of_sums += i
+
+square_of_sums = square_of_sums**2
+
+result = square_of_sums - sum_of_squares
+
+print(result)
