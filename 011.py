dbe601d9058fd44957edd15dd085cd17168caa38
Atheri <corylotze@gmail.com>
2015-07-21 04:22:39 -0700

    Problem 11 done. Changed file names from single digit to triple.

---
+++ 011.py
@@ -0,0 +1,90 @@
+""" 
+Problem: Find the largest product of 4 adjacent numbers in a grid of numbers
+(up, down, left, right, diagonal). 
+
+My implementation includes products that are close to edges (which use as many
+adjacent as possible, 4, 3, 2, 1). Including these made the solution a little
+less pretty. 
+
+Something I know that could be optimized is that I calculate all the directions
+for every number, which results in quite a few duplicate calculations. The
+Right product for (0,0) is the same as the left product for (0,3).
+"""
+
+import time
+
+def calc_max_prod(row, col, n):
+    direction_prods = [1 for x in range(0,8)]
+
+    for i in range(0,4):
+        # Up
+        if row-i >= 0:
+            direction_prods[0] *= grid[row-i][col]
+        # Down
+        if row+i < n:
+            direction_prods[1] *= grid[row+i][col]
+        # Right
+        if col+i < n:
+            direction_prods[2] *= grid[row][col+i]
+        # Left
+        if col-i >= 0:
+            direction_prods[3] *= grid[row][col-i]
+        # Up Right
+        if row-i >= 0 and col+i < n:
+            direction_prods[4] *= grid[row-i][col+i]
+        # Up Left
+        if row-i >= 0 and col-i >= 0:
+            direction_prods[5] *= grid[row-i][col-i]
+        # Down Right
+        if row+i < n and col+i < n:
+            direction_prods[6] *= grid[row+i][col+i]
+        # Down Left
+        if row+i < n and col-i >= 0:
+            direction_prods[7] *= grid[row+i][col-i]
+    return max(direction_prods)
+
+# calc_max_prod - End #
+
+# Main #
+input = []
+input.append("08 02 22 97 38 15 00 40 00 75 04 05 07 78 52 12 50 77 91 08")
+input.append("49 49 99 40 17 81 18 57 60 87 17 40 98 43 69 48 04 56 62 00")
+input.append("81 49 31 73 55 79 14 29 93 71 40 67 53 88 30 03 49 13 36 65")
+input.append("52 70 95 23 04 60 11 42 69 24 68 56 01 32 56 71 37 02 36 91")
+input.append("22 31 16 71 51 67 63 89 41 92 36 54 22 40 40 28 66 33 13 80")
+input.append("24 47 32 60 99 03 45 02 44 75 33 53 78 36 84 20 35 17 12 50")
+input.append("32 98 81 28 64 23 67 10 26 38 40 67 59 54 70 66 18 38 64 70")
+input.append("67 26 20 68 02 62 12 20 95 63 94 39 63 08 40 91 66 49 94 21")
+input.append("24 55 58 05 66 73 99 26 97 17 78 78 96 83 14 88 34 89 63 72")
+input.append("21 36 23 09 75 00 76 44 20 45 35 14 00 61 33 97 34 31 33 95")
+input.append("78 17 53 28 22 75 31 67 15 94 03 80 04 62 16 14 09 53 56 92")
+input.append("16 39 05 42 96 35 31 47 55 58 88 24 00 17 54 24 36 29 85 57")
+input.append("86 56 00 48 35 71 89 07 05 44 44 37 44 60 21 58 51 54 17 58")
+input.append("19 80 81 68 05 94 47 69 28 73 92 13 86 52 17 77 04 89 55 40")
+input.append("04 52 08 83 97 35 99 16 07 97 57 32 16 26 26 79 33 27 98 66")
+input.append("88 36 68 87 57 62 20 72 03 46 33 67 46 55 12 32 63 93 53 69")
+input.append("04 42 16 73 38 25 39 11 24 94 72 18 08 46 29 32 40 62 76 36")
+input.append("20 69 36 41 72 30 23 88 34 62 99 69 82 67 59 85 74 04 36 16")
+input.append("20 73 35 29 78 31 90 01 74 31 49 71 48 86 81 16 23 57 05 54")
+input.append("01 70 54 71 83 51 54 69 16 92 33 48 61 43 52 01 89 19 67 48")
+
+start = time.time()
+
+# Store input in a list of lists, acces by grid[row][col]
+grid = []
+for i in range(0,20):
+    row = [int(x) for x in input[i].split()]
+    grid.append(row)
+
+n = 20
+largest = 0
+for i, row in enumerate(grid):
+    for j, num in enumerate(row):
+        max_prod = calc_max_prod(i, j, n)
+        if max_prod > largest:
+            largest = max_prod
+print(largest)
+end = time.time()
+print("time:", end-start, "seconds")
+
+# Main - End #
