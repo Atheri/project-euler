43b22a38c4078ab4f9d1030db6e78f213dc39203
Atheri <corylotze@gmail.com>
2015-07-26 23:27:14 -0700

    Problem 22 done.

--- 022/022.py
+++ 022/022.py
@@ -1,12 +1,26 @@
+"""
+runtime: 0.0083 seconds
+"""
+
 import time
 
+def alphabet_value(string):
+    total = 0
+    for c in string:
+        total += ord(c)-ord('A')+1
+    return total
+        
 start = time.time()
 
 f = open('p022_names.txt', 'r')
 names = f.read().replace('"', '').split(',')
-
 f.close()
 
-print(names)
+names.sort()
+total = 0
+for i, name in enumerate(names):
+    total += alphabet_value(name) * (i+1)
+
+print("results:", total)
 end = time.time()
 print("time:", end-start, "seconds")
